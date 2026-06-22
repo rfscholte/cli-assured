36cd6076244cd79ca3ac76ad5e739c43c4ca0238
Peter Palaga <ppalaga@redhat.com>
2026-06-22 14:48:50 +0200

    Document org.cliassured.CommandSpec.stdin(Consumer<OutputStream>)

---
+++ docs/modules/ROOT/examples/j21/WriteToStdinTest.java
@@ -0,0 +1,45 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025 CLI Assured contributors as indicated by the @author tags
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.cliassured.test.j21.docs;
+
+// tag::imports[]
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.charset.StandardCharsets;
+import org.cliassured.CliAssured;
+// end::imports[]
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+public class WriteToStdinTest {
+
+    @Test
+    @DisabledOnOs(OS.WINDOWS)
+    void writeToStdin() {
+        // @formatter:off
+        // tag::snippet[]
+        CliAssured
+            // cat with no arguments reads from stdin
+            .command("cat")
+            // Write bytes to the process' stdin
+            .stdin(out -> {
+                try {
+                    out.write("Hello from stdin!".getBytes(StandardCharsets.UTF_8));
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            })
+            .then()
+                .stdout()
+                    // The data written to stdin appears in stdout
+                    .hasLines("Hello from stdin!")
+            .execute()
+            .assertSuccess();
+        // end::snippet[]
+        // @formatter:on
+    }
+
+}
